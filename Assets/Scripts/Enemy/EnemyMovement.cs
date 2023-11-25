26de8f1697d68566e3d19f1858f80b2327ca69e9
Dmytro Hladkyi <dimkabarry@gmail.com>
2023-11-25 19:57:10 +0100

    Upgraded enemy AI

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -2,19 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyMovement : MonoBehaviour
 {
     private Rigidbody2D _rb;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotationSpeed;
+    [SerializeField] private float directionChangeRate;
+    [SerializeField] private float screenBorder;
     private EnemyAwarenessController _enemyAwarenessController;
     private Vector2 _targetDirection;
+    private Camera _camera;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _enemyAwarenessController = GetComponent<EnemyAwarenessController>();
+        _targetDirection = transform.up;
+        _camera = Camera.main;
     }
 
 
@@ -33,23 +39,52 @@ public class EnemyMovement : MonoBehaviour
 
     private void UpdateTargetDirection()
     {
+        HandleRandomDirection();
+        HandlePlayerTargeting();
+        HandleEnemyOffScreen();
+    }
+
+    private void HandleRandomDirection()
+    {
+        directionChangeRate -= Time.deltaTime;
+        
+        if (directionChangeRate <= 0)
+        {
+            float angleChange = Random.Range(-90f, 90f);
+            Quaternion rotation = Quaternion.AngleAxis(angleChange, transform.forward);
+            _targetDirection = rotation * _targetDirection;
+
+            directionChangeRate = Random.Range(1f, 5f);
+        }
+    }
+
+    private void HandlePlayerTargeting()
+    {
         if (_enemyAwarenessController.awareOfPlayer)
         {
             _targetDirection = _enemyAwarenessController.directionToPlayer;
         }
-        else
+    }
+
+    private void HandleEnemyOffScreen()
+    {
+        Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
+
+        if ((screenPosition.x < screenBorder && _targetDirection.x < 0) ||
+            (screenPosition.x > _camera.pixelWidth - screenBorder && _targetDirection.x > 0))
         {
-            _targetDirection = Vector2.zero;
+            _targetDirection = new Vector2(-_targetDirection.x, _targetDirection.y);
+        }
+
+        if ((screenPosition.y < screenBorder && _targetDirection.y < 0) ||
+            (screenPosition.y > _camera.pixelHeight - screenBorder && _targetDirection.y > 0))
+        {
+            _targetDirection = new Vector2(_targetDirection.x, -_targetDirection.y);
         }
     }
 
     private void RotateTowardsTarget()
     {
-        if (_targetDirection == Vector2.zero)
-        {
-            return;
-        }
-
         Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetDirection);
         Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         
@@ -57,14 +92,7 @@ public class EnemyMovement : MonoBehaviour
     }
 
     private void SetVelocity()
-    {
-        if (_targetDirection == Vector2.zero)
-        {
-            _rb.velocity = Vector2.zero;
-        }
-        else
-        {
-            _rb.velocity = transform.up * moveSpeed;
-        }
+    { 
+        _rb.velocity = transform.up * moveSpeed;
     }
 }
